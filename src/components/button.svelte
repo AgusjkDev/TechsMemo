2df08b5c807764d1d4df12643bf53e21e9db2046
AgusjkDev <agusjkdev7@gmail.com>
2023-12-23 14:39:10 -0300

    ⚒️ Update button styles

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -2,6 +2,9 @@
     export let onClick;
 </script>
 
-<button class="py-2.5 w-36 font-medium md:text-sm rounded-md bg-slate-900" on:click={onClick}>
+<button
+    class="py-2.5 w-36 font-medium md:text-sm rounded-md bg-slate-900 transition-colors duration-300 hover:bg-slate-800"
+    on:click={onClick}
+>
     <slot />
 </button>
